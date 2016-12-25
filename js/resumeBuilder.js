8fc39a4f615134ee85643b1009ae52fa998bb94d
Niwat Jansutep <watniwat@hotmail.com>
2016-12-25 11:42:03 +0700

    Add formatted name and formatted role on header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,26 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+//$("#main").append("Niwat");
+
+var awesomeThought = "I am Niwat and I am AWESOME!";
+
+console.log(awesomeThought);
+
+var funThought = awesomeThought.replace("AWESOME", "FUN");
+
+console.log(funThought);
+
+$("#main").append(funThought);
+
+var name = "Niwat Jansutep";
+var role = "Full-stack Developer";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").append(formattedName); 
+$("#header").append(formattedRole);
+
+/*$("#header").prepend(formattedName); 
+$("#header").prepend(formattedRole);*/
